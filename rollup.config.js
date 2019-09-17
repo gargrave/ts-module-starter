d4280a349dc81fecd2a44fadbecae4cc3fccf9cb
Gabe Hargrave <gkhargrave@gmail.com>
2019-09-16 21:14:39 -0700

    Build config updates

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,40 @@ import commonjs from 'rollup-plugin-commonjs'
 
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve({
-      extensions: ['.ts', '.tsx'],
-    }),
-    commonjs({
-      namedExports: { react: ['createElement'] },
-    }),
-    babel({
-      extensions: ['.ts', '.tsx'],
-      include: ['src/**/*'],
-    }),
-  ],
-  external: ['object-assign', 'prop-types/checkPropTypes'],
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: pkg.browser,
+        format: 'umd',
+        globals: { react: 'react' },
+        name: 'tsModuleStarter',
+      },
+    ],
+    external: ['react', 'object-assign', 'prop-types/checkPropTypes'],
+    plugins: [
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      commonjs({
+        namedExports: { react: ['createElement'] },
+      }),
+      babel({
+        extensions: ['.ts', '.tsx'],
+        include: ['src/**/*'],
+        runtimeHelpers: true,
+      }),
+    ],
+  },
+]
